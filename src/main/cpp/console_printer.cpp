055a87d2d19eea4143b1c5156fa386605544071e
nespjin <1756404649@qq.com>
2023-02-11 21:40:01 +0800

    fix(*): reset the color when print info or debug level message.

--- src/main/cpp/console_printer.cpp
+++ src/main/cpp/console_printer.cpp
@@ -18,15 +18,15 @@
 using namespace NespLogger;
 
 //the following are UBUNTU/LINUX ONLY terminal color codes.
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
+#define RESET   "\033[0m\033[0m"
+#define BLACK   "\033[0m\033[30m"      /* Black */
+#define RED     "\033[0m\033[31m"      /* Red */
+#define GREEN   "\033[0m\033[32m"      /* Green */
+#define YELLOW  "\033[0m\033[33m"      /* Yellow */
+#define BLUE    "\033[0m\033[34m"      /* Blue */
+#define MAGENTA "\033[0m\033[35m"      /* Magenta */
+#define CYAN    "\033[0m\033[36m"      /* Cyan */
+#define WHITE   "\033[0m\033[37m"      /* White */
 #define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
@@ -39,7 +39,9 @@ using namespace NespLogger;
 void ConsolePrinter::print(Logger::LogRecord logRecord) {
     const Logger::LogRecord record = logRecord;
     string color;
-    if (record.level == Logger::Level::WARN) {
+    if (record.level == Logger::Level::INFO || record.level == Logger::Level::DEBUG) {
+        color = RESET;
+    } else if (record.level == Logger::Level::WARN) {
         color = YELLOW;
     } else if (record.level == Logger::Level::ERROR) {
         color = RED;
